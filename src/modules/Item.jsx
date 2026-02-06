2271dfd8097581444000e2b1de329cb3fa444ef7
cucu0628 <adambence2007@gmail.com>
2026-02-06 10:16:49 +0100

    card

--- src/modules/Item.jsx
+++ src/modules/Item.jsx
@@ -1,12 +1,17 @@
+import Style from "./Item.module.css"
+
 const Item = ({theme, date, deadline, description, subject, type}) =>{
     return(
         <>
-            <p>{theme}</p>
+        <div className={Style.card}>
+            <p className={`${Style.theme} ${Style.fullWidth}`}>{theme}</p>
             <p>{date}</p>
             <p>{deadline}</p>
             <p>{description}</p>
             <p>{subject}</p>
             <p>{type}</p>
+        </div>
+            
         </>
     )
 }
