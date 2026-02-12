766770a493f50c3def240697b3b7675a8f871982
cucu0628 <adambence2007@gmail.com>
2026-02-12 19:46:38 +0100

    fix

--- src/modules/Item.jsx
+++ src/modules/Item.jsx
@@ -10,14 +10,14 @@ const Item = ({theme, date, deadline, description, subject, type}) =>{
         <>
         <div className={done == true ? Style.done : Style.notDone}>
             <div className={`${Style.theme} ${Style.fullWidth}`}>
-                <p className={Style.text}>{subject}</p>
+                <p className={`${Style.text} ${Style.check}`}>{subject}</p>
                 <p className={Style.icon}>{type == "Szóbeli" ? <RiSpeakFill /> : <BsChatRightTextFill />}</p>
             </div>
             <h1 className={Style.fullWidth}>Téma: {theme}</h1>
             <p className={Style.fullWidth}>{date}</p>
             <p className={Style.fullWidth}>{deadline}</p>
             <p className={Style.fullWidth}>{description}</p>
-            <p className={`${Style.fullWidth} ${Style.check}`}><input type="checkbox" checked={done} onChange={() => setDone(!done)}/> Kész</p>
+            <p className={`${Style.fullWidth} ${Style.check}`}><input type="checkbox" onChange={() => setDone(!done)}/> Kész</p>
             
            
         </div>
