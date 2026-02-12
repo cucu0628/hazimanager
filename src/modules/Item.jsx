f06d03f6dbeedca6e40bc69c5ebf84a4ebfa1a03
cucu0628 <adambence2007@gmail.com>
2026-02-12 19:33:39 +0100

    line

--- src/modules/Item.jsx
+++ src/modules/Item.jsx
@@ -1,18 +1,23 @@
+import { useState } from 'react'
+import { RiSpeakFill } from "react-icons/ri";
+import { BsChatRightTextFill } from "react-icons/bs";
+
 import Style from "./Item.module.css"
 
 const Item = ({theme, date, deadline, description, subject, type}) =>{
+    const [done, setDone] = useState(false);
     return(
         <>
-        <div className={Style.card}>
+        <div className={done == true ? Style.done : Style.notDone}>
             <div className={`${Style.theme} ${Style.fullWidth}`}>
-                
                 <p className={Style.text}>{subject}</p>
-                <p className={Style.icon}>{type}</p>
+                <p className={Style.icon}>{type == "Szóbeli" ? <RiSpeakFill /> : <BsChatRightTextFill />}</p>
             </div>
             <h1 className={Style.fullWidth}>Téma: {theme}</h1>
-            <p>{date}</p>
-            <p>{deadline}</p>
-            <p>{description}</p>
+            <p className={Style.fullWidth}>{date}</p>
+            <p className={Style.fullWidth}>{deadline}</p>
+            <p className={Style.fullWidth}>{description}</p>
+            <p className={`${Style.fullWidth} ${Style.check}`}><input type="checkbox" checked={done} onChange={() => setDone(!done)}/> Kész</p>
             
            
         </div>
