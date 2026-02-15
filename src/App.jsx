3f2120856f558d1ef3f6beed1202920266691c08
cucu0628 <adambence2007@gmail.com>
2026-02-15 10:42:57 +0100

    database

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Form from './modules/Form'
 import TopBar from './modules/TopBar'
@@ -6,6 +6,17 @@ import List from './modules/List'
 
 function App() {
   const [data, setData] = useState([])
+
+  const loadData = async () => {
+    const res = await fetch("http://localhost:8080/keres");
+    const data = await res.json();
+    setData(data);
+  };
+
+  
+  useEffect(() => {
+    loadData();
+  }, []);
 
   const addData = (actualData) => {
     setData((prevData) => [...prevData, actualData])
